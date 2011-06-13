944bcb2ae857d323d349c13c47a6c964d988c6a9
FriedRice <n.mclaughlin22@yahoo.com>
2011-06-13 11:16:45 -0400

    Working LCM, but poor algorithm

--- gcf_lcm.py
+++ gcf_lcm.py
@@ -3,7 +3,7 @@ class NumOperations:
 	def __init__(self, num1, num2):
 		primef1 = self.factorize([num1])
 		primef2 = self.factorize([num2])
-		print self.getLCM()
+		print self.getLCM(primef1, primef2)
 	
 	def factorize(self, num):
 		for x in num:
@@ -22,28 +22,28 @@ class NumOperations:
 		else:
 			return [x, num/x]
 
-	def getLCM(self):
+	def getLCM(self, primef1, primef2):
 		lcmpf = []
 		lcm = 1
 		checked = []
 		
-		for x in self.primef1:
+		for x in primef1:
 			if x not in checked:
-				if self.primef1.count(x) > self.primef2.count(x):
-					lcmpf.extend([x] * self.primef1.count(x))
+				if primef1.count(x) > primef2.count(x):
+					lcmpf.extend([x] * primef1.count(x))
 				else:
-					lcmpf.extend([x] * self.primef2.count(x))
+					lcmpf.extend([x] * primef2.count(x))
 				checked.append(x)
 				
-		for x in self.primef2:
+		for x in primef2:
 			if x not in checked:
-				lcmpf.extend([x] * self.primef2.count(x))
+				lcmpf.extend([x] * primef2.count(x))
 				checked.append(x)
 		
-		for x in lcm:
+		for x in lcmpf:
 			lcm *= x
 		return lcm
 		
 
 if __name__ == "__main__":
-	NumOperations(60,54)
+	NumOperations(2578, 12348)
