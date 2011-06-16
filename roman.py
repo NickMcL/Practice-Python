598a152971029eb64e639d00b70a7248478007e1
FriedRice <n.mclaughlin22@yahoo.com>
2011-06-16 11:23:35 -0400

    roman passes current romantest, but some bugs still need to be addressed.

--- roman.py
+++ roman.py
@@ -1,4 +1,5 @@
 """Convert to and from Roman numeral"""
+import re
 
 
 #Define exceptions
@@ -8,11 +9,53 @@ class NotIntegerError(RomanError): pass
 class InvalidRomanNumeralError(RomanError): pass
 
 
+#Define digit mapping
+romanNumeralMap = (('M',  1000),
+                   ('CM', 900),
+                   ('D',  500),
+                   ('CD', 400),
+                   ('C',  100),
+                   ('XC', 90),
+                   ('L',  50),
+                   ('XL', 40),
+                   ('X',  10),
+                   ('IX', 9),
+                   ('V',  5),
+                   ('IV', 4),
+                   ('I',  1))
+
+
 def toRoman(n):
     """convert integer to Roman numeral"""
-    pass
+    if not (0 < n < 4000):
+        raise OutOfRangeError, "number out of range (must be 1..3999)"
+    if int(n) <> n:
+        raise NotIntegerError, "non-integers can not be converted"
+
+
+    result = ""
+    for numeral, integer in romanNumeralMap:
+        while n >= integer:
+            result += numeral
+            n -= integer
+    return result
+
+
+#Define pattern to detect valid Roman numerals
+romanNumeralPattern = \
+'^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$'
 
 
 def fromRoman(s):
     """convert Roman numeral to integer"""
-    pass
+    if not re.search(romanNumeralPattern, s):
+        raise InvalidRomanNumeralError, 'Invalid Roman numeral: %s' % s
+
+
+    result = 0
+    index = 0
+    for numeral, integer in romanNumeralMap:
+        while s[index:index+len(numeral)] == numeral:
+            result += integer
+            index += len(numeral)
+    return result
