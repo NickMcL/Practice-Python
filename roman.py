23e7288fa6359f3be4841e23ce69783616eaa486
FriedRice <n.mclaughlin22@yahoo.com>
2011-06-16 10:44:58 -0400

    First commit for roman.

---
+++ roman.py
@@ -0,0 +1,18 @@
+"""Convert to and from Roman numeral"""
+
+
+#Define exceptions
+class RomanError(Exception): pass
+class OutOfRangeError(RomanError): pass
+class NotIntegerError(RomanError): pass
+class InvalidRomanNumeralError(RomanError): pass
+
+
+def toRoman(n):
+    """convert integer to Roman numeral"""
+    pass
+
+
+def fromRoman(s):
+    """convert Roman numeral to integer"""
+    pass
