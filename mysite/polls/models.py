85f0b473d24b48fb3e6711eb6e352782e47ec52f
FriedRice <n.mclaughlin22@yahoo.com>
2011-06-20 12:17:23 -0400

    Toyed with several admin options to change the look of the webpage

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -13,6 +13,7 @@ class Poll(models.Model):
 
     def was_published_today(self):
         return self.pub_date.date() == datetime.date.today()
+    was_published_today.short_description = 'Published today?'
 
 
 class Choice(models.Model):
