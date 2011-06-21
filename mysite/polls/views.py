eecc43ecda0a6e7956576c1ea7b68a276755521d
FriedRice <n.mclaughlin22@yahoo.com>
2011-06-21 12:37:26 -0400

    Fixed typos to correct HTML display.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,7 +5,6 @@ from django.template import RequestContext
 from polls.models import Choice, Poll
 
 
-
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('pub_date')[:5]
     return render_to_response('polls/index.html', {'latest_poll_list':
@@ -19,7 +18,8 @@ def detail(request, poll_id):
 
 
 def results(request, poll_id):
-    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    return render_to_response('polls/results.html', {'poll': p})
 
 
 def vote(request, poll_id):
@@ -35,5 +35,5 @@ def vote(request, poll_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResonseRedirect(reverse('polls.views.results',
+        return HttpResponseRedirect(reverse('polls.views.results',
                                    args=(p.id,)))
